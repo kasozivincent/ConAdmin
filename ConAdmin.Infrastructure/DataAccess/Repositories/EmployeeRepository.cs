d23ed393f8932ee8c393f6e8077072a970eccb38
Kasozi Vincent <kasozivincentug507@gmail.com>
2023-09-21 23:08:14 +0100

    Feat: Add EmployeeRepository

---
+++ ConAdmin.Infrastructure/DataAccess/Repositories/EmployeeRepository.cs
@@ -0,0 +1,16 @@
+﻿using ConAdmin.Domain.Employees;
+
+namespace ConAdmin.Infrastructure.DataAccess.Repositories;
+
+public class EmployeeRepository : Repository<Employee>, IEmployeeRepository
+{
+    public IEnumerable<Employee> GetConstructionAdministrators()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Employee> GetPrincipals()
+    {
+        throw new NotImplementedException();
+    }
+}
