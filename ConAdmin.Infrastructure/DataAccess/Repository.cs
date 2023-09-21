6e0a697717c2f2b3d440f02a9eec6fd2fd082216
Kasozi Vincent <kasozivincentug507@gmail.com>
2023-09-21 23:04:04 +0100

    Feat: Repository class

---
+++ ConAdmin.Infrastructure/DataAccess/Repository.cs
@@ -0,0 +1,47 @@
+﻿using ConAdmin.Domain;
+
+namespace ConAdmin.Infrastructure.DataAccess;
+
+public class Repository<TEntity> : IRepository<TEntity> where TEntity : EntityBase
+{
+    public TEntity GetBy(object id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<TEntity> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<TEntity> FindBy(Func<TEntity, bool> predicate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Add(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddRange(IEnumerable<TEntity> entities)
+    {
+        throw new NotImplementedException();
+    }
+
+    public TEntity this[object id]
+    {
+        get => throw new NotImplementedException();
+        set => throw new NotImplementedException();
+    }
+
+    public void Remove(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveRange(IEnumerable<TEntity> entities)
+    {
+        throw new NotImplementedException();
+    }
+}
