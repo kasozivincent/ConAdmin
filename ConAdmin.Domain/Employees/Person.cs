937ec6c134168be8be22d2ae324b24d3280dc345
Kasozi Vincent <kasozivincentug507@gmail.com>
2023-09-21 17:59:40 +0100

    Fix: Remove buggy ctors

--- ConAdmin.Domain/Employees/Person.cs
+++ ConAdmin.Domain/Employees/Person.cs
@@ -5,8 +5,6 @@ public abstract class Person : EntityBase
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Initials { get; set; }
-    protected Person() : this(null) { }
-    protected Person(object? key) : this(key, string.Empty, string.Empty) { }
     protected Person(object? key, string firstName, string lastName) : base(key)
     {
         FirstName = firstName;
