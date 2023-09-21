1e2f3d4ff4fbbc67467bd2281ce3b26d7d3d16e8
Kasozi Vincent <kasozivincentug507@gmail.com>
2023-09-21 17:51:57 +0100

    Feat: Add Person class

---
+++ ConAdmin.Domain/Employees/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace ConAdmin.Domain.Employees;
+
+public abstract class Person : EntityBase
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Initials { get; set; }
+ 
+    protected Person() : this(null) { }
+    protected Person(object key) : this(key, string.Empty, string.Empty) { }
+    protected Person(object key, string firstName, string lastName) : base(key)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Initials = string.Empty;
+    }
+}
