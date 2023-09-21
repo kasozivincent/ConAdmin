1c86ada513fdb1c0c3d9de434da19c5d64d82b61
Kasozi Vincent <kasozivincentug507@gmail.com>
2023-09-21 19:26:25 +0100

    Feat: Add Contract class

---
+++ ConAdmin.Domain/Projects/Contract.cs
@@ -0,0 +1,25 @@
+﻿using ConAdmin.Domain.Companies;
+
+namespace ConAdmin.Domain.Projects;
+
+public class Contract : EntityBase
+{
+    public Company Contractor { get; set; }
+    public string ScopeOfWork { get; set; }
+    public string BidPackageNumber { get; set; }
+    
+    public DateTime? ContractDate { get; set; }
+    
+    public DateTime? NoticeToProceedDate { get; set; }
+    public decimal ContractAmount { get; set; }
+ 
+    public Contract(object key) : base(key)
+    {
+        //Contractor = new Company();
+        ScopeOfWork = string.Empty;
+        BidPackageNumber = string.Empty;
+        ContractAmount = 0;
+    }
+ 
+   
+}
