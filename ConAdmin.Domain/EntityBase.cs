f3892b88d86fc4c5ea825629e82595b16a642647
Kasozi Vincent <kasozivincentug507@gmail.com>
2023-09-21 17:53:44 +0100

    Fix: Fix spacing in EntityBase

--- ConAdmin.Domain/EntityBase.cs
+++ ConAdmin.Domain/EntityBase.cs
@@ -5,16 +5,12 @@ public abstract class EntityBase
     protected EntityBase() : this(null){}
     protected EntityBase(object? key) => Key = key;
     public object? Key { get; }
-    
     public override bool Equals(object? entity)
         => entity is EntityBase other && this == other;
-    
     public override int GetHashCode()
         => this.Key.GetHashCode();
-    
     public static bool operator ==(EntityBase base1, EntityBase base2)
         => base1.Key == base2.Key;
-    
     public static bool operator !=(EntityBase base1, EntityBase base2)
         => !(base1 == base2);
 }
