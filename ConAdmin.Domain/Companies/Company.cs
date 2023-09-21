4ce9ddd40c60dc0ffbdbd256a6026056c60b341f
Kasozi Vincent <kasozivincentug507@gmail.com>
2023-09-21 19:26:08 +0100

    Feat: Add company class

---
+++ ConAdmin.Domain/Companies/Company.cs
@@ -0,0 +1,16 @@
+﻿namespace ConAdmin.Domain.Companies;
+
+public class Company : EntityBase
+{
+    public string Name { get; set; }
+    private string Abbreviation { get; set; }
+    private Address Address { get; set; }
+
+    public Company(object key, string name, string abbrev, Address address) : base(key)
+    {
+        Name = name;
+        Abbreviation = abbrev;
+        Address = address;
+    }
+ 
+}
