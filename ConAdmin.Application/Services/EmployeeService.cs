f85b4b6b9269d3c40cba84a46369ec7d44d233d9
Kasozi Vincent <kasozivincentug507@gmail.com>
2023-09-22 00:03:50 +0100

    Feat: Add project service

--- ConAdmin.Application/Services/EmployeeService.cs
+++ ConAdmin.Application/Services/EmployeeService.cs
@@ -1,6 +1,18 @@
-﻿namespace ConAdmin.Application.Services;
+﻿using ConAdmin.Domain.Employees;
+using ConAdmin.Infrastructure.DataAccess;
+
+namespace ConAdmin.Application.Services;
 
 public class EmployeeService
 {
-    
+    private readonly IUnitOfWork _uow;
+
+    public EmployeeService(IUnitOfWork uow)
+        => this._uow = uow;
+
+    public IEnumerable<Employee> GetConstructionAdministrators()
+        => _uow.Employees.GetConstructionAdministrators();
+
+    public IEnumerable<Employee> GetPrincipals()
+        => _uow.Employees.GetPrincipals();
 }
--- ConAdmin.Application/Services/ProjectService.cs
+++ ConAdmin.Application/Services/ProjectService.cs
@@ -1,6 +1,18 @@
-﻿namespace ConAdmin.Application.Services;
+﻿using ConAdmin.Domain.Projects;
+using ConAdmin.Infrastructure.DataAccess;
+
+namespace ConAdmin.Application.Services;
 
 public class ProjectService
 {
-    
+    private readonly IUnitOfWork _uow;
+
+    public ProjectService(IUnitOfWork uow)
+        => this._uow = uow;
+
+    public IEnumerable<Project> GetAllProjects()
+        => _uow.Projects.GetAll();
+
+    public IEnumerable<MarketSegment> GetMarketSegments()
+        => _uow.Projects.FindAllMarketSegments();
 }
