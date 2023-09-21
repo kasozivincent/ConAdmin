30a90ac657e97e7c45d8cda10c91db17a6d0a8c7
Kasozi Vincent <kasozivincentug507@gmail.com>
2023-09-21 19:32:19 +0100

    Feat: Add IProjectRepository interface

---
+++ ConAdmin.Domain/Projects/IProjectRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace ConAdmin.Domain.Projects;
+
+public interface IProjectRepository : IRepository <Project> 
+{
+    IEnumerable<Project> FindBy(IEnumerable<MarketSegment> segments, bool completed);
+    Project FindBy(string projectNumber);
+    IEnumerable<MarketSegment> FindAllMarketSegments();
+}
