323d564d6fadb5001bd741c20d8501ff4cd748f7
Kasozi Vincent <kasozivincentug507@gmail.com>
2023-09-21 23:05:00 +0100

    Feat: Add Lib Registration

---
+++ ConAdmin.Infrastructure/DataAccess/ServiceCollectionExt.cs
@@ -0,0 +1,16 @@
+﻿using ConAdmin.Domain.Employees;
+using ConAdmin.Domain.Projects;
+using ConAdmin.Infrastructure.DataAccess.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConAdmin.Infrastructure.DataAccess;
+
+public static class ServiceCollectionExt
+{
+    public static IServiceCollection RegisterDataAccess(this IServiceCollection services)
+    {
+        services.AddScoped<IProjectRepository, ProjectRepository>();
+        services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+        return services;
+    }
+}
