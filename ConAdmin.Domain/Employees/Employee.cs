43721dd180e79672e3bf87b7e4fe4e12d63b8655
Kasozi Vincent <kasozivincentug507@gmail.com>
2023-09-21 18:00:35 +0100

    Feat: Add Employee class

---
+++ ConAdmin.Domain/Employees/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace ConAdmin.Domain.Employees;
+
+public class Employee : Person
+{
+    private string JobTitle { get; set; }
+ 
+    public Employee(object key, string firstName, string lastName) : base(key, firstName, lastName)
+        =>  JobTitle = string.Empty;
+}
