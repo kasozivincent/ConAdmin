c833c4232a1a9f63224df48b9a56f791442c901e
Kasozi Vincent <kasozivincentug507@gmail.com>
2023-09-21 19:06:59 +0100

    Feat: Add MarketSegment class

---
+++ ConAdmin.Domain/Projects/MarketSector.cs
@@ -0,0 +1,13 @@
+﻿namespace ConAdmin.Domain.Projects;
+
+public class MarketSector : EntityBase
+{
+    public string Name { set; get; }
+    public IEnumerable<MarketSegment> Segments { get; }
+ 
+    public MarketSector(object? key, string name) : base(key)
+    {
+        Name = name;
+        Segments = new List<MarketSegment>();
+    }
+}
---
+++ ConAdmin.Domain/Projects/Segment.cs
@@ -0,0 +1,15 @@
+﻿namespace ConAdmin.Domain.Projects;
+public class MarketSegment : EntityBase
+{
+    public MarketSector ParentSector { get; }
+    public string Name { get; set; }
+    public string Code { set; get; }
+ 
+    public MarketSegment(object? key, MarketSector parentSector, string name,
+        string code) : base(key)
+    {
+        ParentSector = parentSector;
+        Name = name;
+        Code = code;
+    }
+}
