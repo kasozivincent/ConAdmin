d4c24b95beaa34669e45afe55d16432ae74dca67
Kasozi Vincent <kasozivincentug507@gmail.com>
2023-09-21 18:35:49 +0100

    Feat: Add IRepository interface

---
+++ ConAdmin.Domain/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace ConAdmin.Domain;
+
+public interface IRepository<T> where T : EntityBase
+{
+    T GetBy(object id);
+    IEnumerable<T> GetAll();
+    void Add(T entity);
+    T this[object id] { get; set; }
+    void Remove(T entity);
+}
